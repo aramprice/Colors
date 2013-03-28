8b5ac28262fd862f2e45cf98824b0fd434a115ae
aram price <me+git@aramprice.com>
2013-03-28 14:52:18 -0700

    Initial Commit

---
+++ Colors/ColorsView.h
@@ -0,0 +1,13 @@
+//
+//  ColorsView.h
+//  Colors
+//
+//  Created by aram price on 28/03/2013.
+//  Copyright (c) 2013 aram price. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface ColorsView : ScreenSaverView
+
+@end
---
+++ Colors/ColorsView.m
@@ -0,0 +1,52 @@
+//
+//  ColorsView.m
+//  Colors
+//
+//  Created by aram price on 28/03/2013.
+//  Copyright (c) 2013 aram price. All rights reserved.
+//
+
+#import "ColorsView.h"
+
+@implementation ColorsView
+
+- (id)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
